3e73f4541bdd4d56e255774f4006141e0bc3250e
wuxinIOS <wuxinIOS@163.com>
2017-06-20 14:45:02 +0800

    自定定义表情键盘项目初始化

--- WXEmoticonKeyboard/ViewController.swift
+++ WXEmoticonKeyboard/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changedKeyboardButton: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var toolBottomContraint: NSLayoutConstraint!
+    
+    
+    lazy var emoticonKeyBoard : UIView! = {
+        
+        let view = UIView(frame: CGRect(x: 0, y: UIScreen.main.bounds.height - 255, width: UIScreen.main.bounds.width, height: 255))
+        view.backgroundColor = UIColor.orange
+        return view
+        
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.becomeFirstResponder()
+        NotificationCenter.default.addObserver(self, selector: #selector(textFileChanged), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+    }
+    
+    @objc fileprivate func textFileChanged(notifiction:Notification) {
+        
+        /**NSDictionary *info = [notif userInfo];
+        NSValue *value = [info objectForKey:UIKeyboardFrameBeginUserInfoKey];
+        CGSize keyboardSize = [value CGRectValue].size;
+        
+        NSLog(@"keyBoard:%f", keyboardSize.height);  //216
+        */
+        
+        let info = notifiction.userInfo
+        
+        print(info ?? "")
+        
+    }
+    
+    
+    
+    @IBAction func changedKeyboardBtn(_ sender: Any) {
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
